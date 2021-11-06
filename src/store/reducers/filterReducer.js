4c9ccbc163d8374ce40f0ed16b5137fa986a7254
furuturu <furufuru@yandex.ru>
2021-11-06 16:39:49 +0300

    fix: import action types

--- src/store/reducers/filterReducer.js
+++ src/store/reducers/filterReducer.js
@@ -1,8 +1,12 @@
-const initialState = { filter: "SHOW_ALL" };
+import { actionTypes } from "../action/actionTypes";
+
+const { SHOW_ALL, SET_FILTER } = actionTypes;
+
+const initialState = { filter: SHOW_ALL };
 
 export const filterReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "SET_FILTER":
+    case SET_FILTER:
       return { ...state, filter: action.payload };
     default:
       return state;
