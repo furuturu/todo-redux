32244a7d03191b1e7494d2baaaaaaed76421f291
furuturu <furufuru@yandex.ru>
2021-11-06 16:42:03 +0300

    fix: change code structure

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -6,34 +6,22 @@ import { ListWrapper } from "./TaskList.styles";
 export const TaskList = () => {
   const todo = useSelector((state) => state.taskReducer.todo);
   const filter = useSelector((state) => state.filterReducer.filter);
-  switch (filter) {
-    case "SHOW_ALL":
-      return (
-        <ListWrapper>
-          {todo.map((task) => (
-            <Task key={task.id} task={task} />
-          ))}
-        </ListWrapper>
-      );
-    case "SHOW_ACTIVE":
-      return (
-        <ListWrapper>
-          {todo
-            .filter((task) => task.isActive === true)
-            .map((task) => (
-              <Task key={task.id} task={task} />
-            ))}
-        </ListWrapper>
-      );
-    case "SHOW_DONE":
-      return (
-        <ListWrapper>
-          {todo
-            .filter((task) => task.isActive === false)
-            .map((task) => (
-              <Task key={task.id} task={task} />
-            ))}
-        </ListWrapper>
-      );
-  }
+  const filterTasksByStatus = (filter, todo) => {
+    switch (filter) {
+      case "SHOW_DONE":
+        return todo.filter((task) => task.isActive === false);
+      case "SHOW_ACTIVE":
+        return todo.filter((task) => task.isActive === true);
+      case "SHOW_ALL":
+        return todo;
+    }
+  };
+  const taskList = filterTasksByStatus(filter, todo);
+  return (
+    <ListWrapper>
+      {taskList.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </ListWrapper>
+  );
 };
